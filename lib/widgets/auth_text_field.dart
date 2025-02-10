64b02f943d5db2b66c76d6d1bd9a2575e1a22fa2
khalil <khawajakhalil3@gmail.com>
2025-02-10 21:16:26 +0300

    styles and conf

--- lib/widgets/auth_text_field.dart
+++ lib/widgets/auth_text_field.dart
@@ -1,47 +1,60 @@
-
 part of 'package:qofi_comp/qofi_comp.dart';
+
 class _AuthTextField extends StatelessWidget {
-  _AuthTextField({super.key, required this.e});
+  _AuthTextField({required this.e});
   final Map e;
   final TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
+    InputBorder outlineInputBorder = e['border'] == "none"
+        ? const OutlineInputBorder(
+      borderSide: BorderSide.none
+    )
+        : OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.sp),
+            borderSide: BorderSide(
+              color: Theme.of(context).colorScheme.error,
+            ),
+          );
     if (e["type"] == "phone") {
       return Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-         if(false) ...[Container(
-            width: 30.w,
-            padding: EdgeInsets.all(0.5.w),
-            decoration: BoxDecoration(
-              color: Theme.of(context).cardColor,
-              borderRadius: BorderRadius.circular(12.sp),
-              border: Border.all(color: const Color(0xffE5E5E5), width: 2),
-            ),
-            child: CountryCodePicker(
-              padding: EdgeInsets.zero,
-              dialogSize: Size(90.w, 80.h),
-              initialSelection: e['initialSelection'] ?? 'PK',
-              boxDecoration: BoxDecoration(
-                color: Theme.of(context).scaffoldBackgroundColor,
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(color: const Color(0xffD6D6D6), width: 2),
+          if (false) ...[
+            Container(
+              width: 30.w,
+              padding: EdgeInsets.all(0.5.w),
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.circular(12.sp),
+                border: Border.all(color: const Color(0xffE5E5E5), width: 2),
               ),
-              onChanged: (value) {
-                e['onChanged']?.call({
-                  "country_code": value.dialCode,
-                });
-              },
-              dialogBackgroundColor: Theme.of(context).scaffoldBackgroundColor,
-              textStyle: TextStyle(
-                  color: Theme.of(context).canvasColor,
-                  fontSize: 14.ft,
-                  fontWeight: FontWeight.w500),
+              child: CountryCodePicker(
+                padding: EdgeInsets.zero,
+                dialogSize: Size(90.w, 80.h),
+                initialSelection: e['initialSelection'] ?? 'PK',
+                boxDecoration: BoxDecoration(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: const Color(0xffD6D6D6), width: 2),
+                ),
+                onChanged: (value) {
+                  e['onChanged']?.call({
+                    "country_code": value.dialCode,
+                  });
+                },
+                dialogBackgroundColor:
+                    Theme.of(context).scaffoldBackgroundColor,
+                textStyle: TextStyle(
+                    color: Theme.of(context).canvasColor,
+                    fontSize: 14.ft,
+                    fontWeight: FontWeight.w500),
+              ),
             ),
-          ),
-          SizedBox(
-            width: 2.w,
-          )],
+            SizedBox(
+              width: 2.w,
+            )
+          ],
           Expanded(
             child: _AuthTextField(e: {
               ...e,
@@ -56,14 +69,14 @@ class _AuthTextField extends StatelessWidget {
           ),
         ],
       );
-    }
-    else if (e['type'] == "dropdown") {
+    } else if (e['type'] == "dropdown") {
       return DropdownButtonFormField(
         icon: SvgPicture.asset("assets/svgs/arrow-down.svg",
             color: Theme.of(context).dividerColor),
         isDense: true,
         dropdownColor: Theme.of(context).cardColor,
         elevation: 0,
+        validator: e['validator'],
         decoration: InputDecoration(
           errorMaxLines: 2,
           prefixIconConstraints: BoxConstraints(maxHeight: 0, maxWidth: 0),
@@ -75,7 +88,7 @@ class _AuthTextField extends StatelessWidget {
               fontSize: 13.ft,
               fontWeight: FontWeight.w500),
           hintStyle: TextStyle(
-              color: HexColor("878787"),
+              color: Theme.of(context).canvasColor,
               fontSize: 14.ft,
               fontWeight: FontWeight.w500),
           filled: e['filled'] ?? true,
@@ -96,25 +109,18 @@ class _AuthTextField extends StatelessWidget {
               color: Color(0xffD6D6D6),
             ),
           ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12.sp),
-            borderSide: BorderSide(
-              color: Theme.of(context).colorScheme.error,
-            ),
-          ),
-          focusedErrorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12.sp),
-            borderSide: BorderSide(
-              color: Theme.of(context).colorScheme.error,
-            ),
-          ),
+          errorBorder: outlineInputBorder,
+          focusedErrorBorder: outlineInputBorder,
         ),
         value: e['value'],
         items: List.from(e['items'].map<DropdownMenuItem>((value) {
           return DropdownMenuItem(
             value: "${value['id']}",
             child: Text("${value['name']}",
-                style: TextStyle(fontWeight: FontWeight.normal)),
+                style: TextStyle(
+                  fontWeight: FontWeight.normal,
+                  color: Theme.of(context).canvasColor,
+                )),
           );
         })),
         padding: EdgeInsets.zero,
@@ -162,11 +168,13 @@ class _AuthTextField extends StatelessWidget {
         filled: e['filled'] ?? true,
         prefixIcon: e["prefix"] == null
             ? null
-            : SvgPicture.asset(
-                e["prefix"],
-                width: 5.5.w,
-                height: 5.5.w,
-              ),
+            : e["prefix"] is String
+                ? SvgPicture.asset(
+                    e["prefix"],
+                    width: 5.5.w,
+                    height: 5.5.w,
+                  )
+                : e["prefix"],
         suffixIcon: e["suffix"] == null
             ? null
             : e['suffix'] is Widget
@@ -199,18 +207,8 @@ class _AuthTextField extends StatelessWidget {
             color: Color(0xffD6D6D6),
           ),
         ),
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.sp),
-          borderSide: BorderSide(
-            color: Theme.of(context).colorScheme.error,
-          ),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(12.sp),
-          borderSide: BorderSide(
-            color: Theme.of(context).colorScheme.error,
-          ),
-        ),
+        errorBorder: outlineInputBorder,
+        focusedErrorBorder: outlineInputBorder,
       ),
     );
   }
